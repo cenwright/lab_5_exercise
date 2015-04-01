f31ee813d4e8ff0827393979f28eb1b24aa415ed
Caitlin Enwright <caitlinenwright@gmail.com>
2015-03-31 18:31:13 -0700

    First Commit

---
+++ shakil_the_dog.rb
@@ -0,0 +1,19 @@
+puts "I'm shakil, say something to me:"
+input = gets.chomp
+# puts input
+	if input == "woof"
+		puts "woof woof woof"
+	elsif input == "shakil stop"
+		puts " "
+	elsif input == "Shakil STOP!"
+		puts " "
+	elsif input == "meow"
+		puts "woof woof woof woof woof"
+	elsif input == "go away"
+		puts "goodbye, i'm leaving the room"
+	elsif input.include? ("treat")
+		puts " "
+	else puts "woof"	
+	
+	end
+	
